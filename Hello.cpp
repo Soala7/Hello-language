99da51e5236fe9dac1db208d09c615974bb090c3
Amachree Soala <nigaamace@gmail.com>
2025-09-03 11:55:52 +0100

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, VS Code and GCC!" << endl;
+    return 0;
+}
---
+++ Mysecond.java
@@ -0,0 +1,9 @@
+//My firstt code/program
+/* i have a background of C/C++ */
+class Mysecond{
+    public static void main(string args[]) {
+    
+            system.out.printin("Hey,Welcome");
+   
+     }//end of main
+}/end of class

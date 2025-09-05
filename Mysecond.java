f89af907dda02456d8653445ef97c03ceb89f1e7
Amachree Soala <nigaamace@gmail.com>
2025-09-05 13:32:07 +0100

    Delete Mysecond.java

--- Mysecond.java
+++
@@ -1,9 +0,0 @@
-//My firstt code/program
-/* i have a background of C/C++ */
-class Mysecond{
-    public static void main(string args[]) {
-    
-            system.out.printin("Hey,Welcome");
-   
-     }//end of main
-}/end of class
